bc1846f2d8af5096959f72f2d159ce6a3e35c9f6
sslsimon <sslzhai@gmail.com>
2013-06-29 13:23:45 +0800

    项目导入

---
+++ Unit1.dfm
@@ -0,0 +1,79 @@
+object Form1: TForm1
+  Left = 192
+  Top = 114
+  Width = 870
+  Height = 640
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  OnShow = FormShow
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 390
+    Top = 8
+    Width = 31
+    Height = 13
+    Caption = 'Label1'
+  end
+  object Button1: TButton
+    Left = 32
+    Top = 512
+    Width = 75
+    Height = 25
+    Caption = #36830#25509
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 216
+    Top = 512
+    Width = 75
+    Height = 25
+    Caption = #19979#36733
+    TabOrder = 1
+    OnClick = Button2Click
+  end
+  object Edit1: TEdit
+    Left = 8
+    Top = 3
+    Width = 369
+    Height = 21
+    TabOrder = 2
+    Text = 'Edit1'
+  end
+  object Memo1: TMemo
+    Left = 8
+    Top = 32
+    Width = 785
+    Height = 225
+    Lines.Strings = (
+      'Memo1')
+    ScrollBars = ssBoth
+    TabOrder = 3
+  end
+  object ProgressBar1: TProgressBar
+    Left = 32
+    Top = 448
+    Width = 769
+    Height = 17
+    TabOrder = 4
+  end
+  object OpenDialog1: TOpenDialog
+    Left = 256
+    Top = 208
+  end
+  object IdFTP1: TIdFTP
+    OnStatus = IdFTP1Status
+    AutoLogin = True
+    ProxySettings.ProxyType = fpcmNone
+    ProxySettings.Port = 0
+    Left = 616
+    Top = 312
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,205 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, IdBaseComponent, IdComponent, IdTCPConnection,
+  IdTCPClient, IdFTP, ComCtrls, IdExplicitTLSClientServerBase;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Edit1: TEdit;
+    Memo1: TMemo;
+    Label1: TLabel;
+    OpenDialog1: TOpenDialog;
+    IdFTP1: TIdFTP;
+    ProgressBar1: TProgressBar;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure IdFTP1Status(ASender: TObject; const AStatus: TIdStatus;
+      const AStatusText: string);
+    procedure FormShow(Sender: TObject);
+  private
+    { Private declarations }
+    function MsgBox(Msg: string; iValue: integer): integer;
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+uses IdFTPList, IdFTPCommon;
+{$R *.dfm}
+
+function TForm1.MsgBox(Msg: string; iValue: integer): integer;
+begin
+  Result := MessageBox(application.Handle, pChar(Msg), '系统信息', iValue + MB_APPLMODAL);
+end;
+
+//设置文件修改时间
+Function SetFileDateTime(FileName : String; NewDateTime : TDateTime): Boolean;
+var FileHandle: Integer;
+    FileTime: TFileTime;
+    LFT: TFileTime;
+    LST: TSystemTime;
+begin
+  Result := False;
+try
+  DecodeDate(NewDateTime, LST.wYear, LST.wMonth, LST.wDay);
+  DecodeTime(NewDateTime, LST.wHour, LST.wMinute, LST.wSecond, LST.wMilliSeconds);
+  IF SystemTimeToFileTime(LST, LFT) Then
+  begin
+    IF LocalFileTimeToFileTime(LFT, FileTime) Then
+    begin
+      FileHandle := FileOpen(FileName, fmOpenReadWrite or fmShareExclusive);
+      IF SetFileTime(FileHandle, NIL, NIL, @FileTime) Then Result := True;
+    end;
+  end;
+finally
+  FileClose(FileHandle);
+end;  
+end; 
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  tr: Tstrings;
+begin //连接
+  tr := TStringlist.Create;
+  try
+    try
+      idftp1.Passive := True;
+      idftp1.Host := '192.180.0.98'; //FTP服务器地址
+      idftp1.Username := 'guest'; //FTP服务器用户名
+      idftp1.Password := ''; //FTP服务器密码
+      idftp1.Connect(); //连接到ftp
+     //idftp1.ChangeDir('client'); //进入到client子目录
+     //idftp1.ChangeDir('..'); //回到上一级目录
+      Edit1.Text := idftp1.RetrieveCurrentDir; //得到初始目录
+      idftp1.List(tr, '', false); //得到client目录下所有文件列表
+      Memo1.Lines.Assign(tr);
+    except
+      on E: Exception do
+      begin
+        MessageBox(Self.Handle, Pchar('系统提示您：' + e.Message), Pchar(self.Caption), MB_OK + MB_ICONERROR);
+         // memo2.Lines.Add('Exception');
+      end;
+    end;
+  finally
+    tr.Free;
+  end;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+  Dir_List: TStringList;
+  i: integer;
+  new_file_datetime:TDateTime;
+begin //下载
+  // if not IdFTP1.Connected then IdFTP1.Connected;
+  //if not IdFTP1.Connected then idftp1.Connect(True);
+  //Dir_List := TStringList.Create;
+ // IdFTP1.Connect();
+  IdFTP1.TransferType := ftASCII; //ftBinary; //指定为二进制文件   或文本文件ftASCII
+  //IdFTP1.List(Dir_List,'',true);
+
+ // idftp1.List(Dir_List, '', true);
+ // Memo1.Clear;
+  //memo1.Lines.Assign(Dir_List);
+
+  IDFTP1.TransferType := ftBinary; //更改传输类型
+  ProgressBar1.Min := 0;
+  ProgressBar1.Max := memo1.Lines.Count;
+                                                        // Memo1.Lines
+  for i := 0 to  memo1.Lines.Count - 1 do
+  begin
+      // memo2.Lines.Add(IdFTP1.DirectoryListing.Items[i].FileName+' ('+IntToStr(IdFTP1.DirectoryListing.Items[i].Size)+' Byte) Start Download....');
+    {   try
+         IdFTP1.Get(IdFTP1.DirectoryListing.Items[i].FileName,'d:\FTPtest\'+IdFTP1.DirectoryListing.Items[i].FileName,true,True); //下载到本地，并为覆盖，且支持断点续传
+       except
+       end;  }
+    // IdFTP1.Get(IdFTP1.DirectoryListing.Items[i].FileName, 'd:\FTPtest\' + IdFTP1.DirectoryListing.Items[i].FileName, true);
+     //ShowMessage(DateTimeToStr(IdFTP1.DirectoryListing.Items[i].ModifiedDate));
+     new_file_datetime:= idFTP1.FileDate(memo1.Lines[i]);
+     IdFTP1.Get(memo1.Lines[i], 'd:\FTPtest\' + memo1.Lines[i], true);
+     SetFileDateTime('d:\FTPtest\' + memo1.Lines[i],new_file_datetime);
+    // ShowMessage(IdFTP1.DirectoryListing.Items[i].FileName);
+    { 原处理过程，带提示可选
+      if FileExists('d:\FTPtest\' + IdFTP1.DirectoryListing.Items[i].FileName) then
+        case MsgBox(IdFTP1.DirectoryListing.Items[i].FileName + '文件已经存在，要续传吗？'#13#10'是——续传'#10#13'否——覆盖'#13#10'取消——取消操作', MB_YESNOCANCEL + MB_ICONINFORMATION) of
+          IDYES: begin
+             //参数说明: 源文件，目标文件,是否覆盖,是否触发异常（True为不触发)。
+              IdFTP1.Get(IdFTP1.DirectoryListing.Items[i].FileName, 'd:\FTPtest\' + IdFTP1.DirectoryListing.Items[i].FileName, False, True);
+            end;
+          IDNO: begin
+              IdFTP1.Get(IdFTP1.DirectoryListing.Items[i].FileName, 'd:\FTPtest\' + IdFTP1.DirectoryListing.Items[i].FileName, true);
+            end;
+          IDCANCEL:
+            begin
+            end;
+        end
+      else
+        IdFTP1.Get(IdFTP1.DirectoryListing.Items[i].FileName, 'd:\FTPtest\' + IdFTP1.DirectoryListing.Items[i].FileName);
+    end;
+     原处理过程结束}
+    ProgressBar1.Position := i + 1;
+  end;
+  MessageBox(Self.Handle, Pchar('系统提示您：本次操作完毕。'), Pchar(self.Caption), MB_OK + MB_ICONINFORMATION);
+  ProgressBar1.Position := 0;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+var
+  fi: string;
+begin //上传
+  if OpenDialog1.Execute then
+  begin
+    fi := OpenDialog1.FileName;
+    IdFTP1.Put(fi, ExtractFileName(fi)); //上传，
+  end;
+end;
+
+procedure TForm1.IdFTP1Status(ASender: TObject; const AStatus: TIdStatus;
+  const AStatusText: string);
+begin
+   // Memo2.Clear;
+  //  memo2.Lines.Add(AStatusText)
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+var
+  tr: Tstrings;
+begin //连接
+{  tr := TStringlist.Create;
+  try
+    try
+      idftp1.Passive := True;
+      idftp1.Host := '192.180.0.98'; //FTP服务器地址
+      idftp1.Username := 'guest'; //FTP服务器用户名
+      idftp1.Password := ''; //FTP服务器密码
+      idftp1.Connect(); //连接到ftp
+     //idftp1.ChangeDir('client'); //进入到client子目录
+     //idftp1.ChangeDir('..'); //回到上一级目录
+      Edit1.Text := idftp1.RetrieveCurrentDir; //得到初始目录
+      idftp1.List(tr, '', true); //得到client目录下所有文件列表
+      Memo1.Lines.Assign(tr);
+     // memo1.Lines.Assign(Dir_List);
+    except
+      on E: Exception do
+      begin
+        MessageBox(Self.Handle, Pchar('系统提示您：' + e.Message), Pchar(self.Caption), MB_OK + MB_ICONERROR);
+         // memo2.Lines.Add('Exception');
+      end;
+    end;
+  finally
+    tr.Free;
+  end;  }
+end;
+
+end.
+
